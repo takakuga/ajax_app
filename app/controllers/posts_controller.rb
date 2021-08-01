061d50ab65176900bff6af4eb30c9a93728f042e
takakuga <sazanami398@gmail.com>
2021-08-02 01:10:11 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,20 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
+  # def new
+  # end　今回は不要のため
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index  #メモを保存した後にトップページへリダイレクトの為追記する
   end
 end
+
+
+
+
+# メモを降順、つまり、投稿の新しい順に表示されるようにする、下記
+# @posts = Post.order(id: "DESC")
